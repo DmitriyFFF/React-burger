b2c09159b71d60bdaa543e8655deb9fef748f3d7
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2022-11-02 00:08:29 +0400

    feat:add Logo and header-structure

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Logo, Icons, Typography, Box } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon, Typography, Box } from '@ya.praktikum/react-developer-burger-ui-components';
+import appHeaderStyles from './AppHeader.module.css';
 
 function AppHeaderLogo () {
   return (
@@ -7,15 +8,66 @@ function AppHeaderLogo () {
   );
 }
 
+/*function NavigationMenu () {
+  return (
+    <nav className={appHeaderStyles.container}>
+      <MenuItems />
+    </nav>
+  );
+}*/
+
+function NavigationMenu () {
+  return (
+    <ul className={appHeaderStyles.menuContainer}>
+      <li className={appHeaderStyles.menuItem}>
+        <BurgerIcon type='primary'/>
+        <p className='text'>Конструктор</p>
+      </li>
+      <li className={appHeaderStyles.menuItem}>
+        <ListIcon type='primary'/>
+        <p className='text'>Лента заказов</p>
+      </li>
+    </ul>
+  );
+}
+
+function Profile () {
+  return (
+    <div className={appHeaderStyles.menuItem}>
+      <ProfileIcon type='primary'/>
+      <p className='text'>Личный кабинет</p>
+    </div>
+  );
+}
+
+/*function MenuItem (props) {
+  return (
+    <ul>
+      <li className='item'>
+        {props.value}
+      </li>
+      <li className='item'>
+        {props.value}
+      </li>
+    </ul>
+  );
+}
+
+function Menu (props) {
+  return (
+    <nav className='menu'>
+      {props.children}
+    </nav>
+  );
+}*/
+
 
 function AppHeader () {
   return (
-    <header className='header'>
-      <Menu>
-        <MenuItem>Конструктор</MenuItem>
-        <MenuItem>Лента заказов</MenuItem>
-      </Menu>
+    <header className={appHeaderStyles.header}>
+      <NavigationMenu />
       <AppHeaderLogo />
+      <Profile />
     </header>
   );
 }
